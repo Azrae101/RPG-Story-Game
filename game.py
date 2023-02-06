148b53153bcf8cc3ebe77a901530ea9a44999eee
Clara <clara.holst@outlook.com>
2023-02-06 10:46:56 +0100

    what

--- game.py
+++ game.py
@@ -33,8 +33,16 @@ while True: # while is running:
     
     user_input = input(" ")
 
+    # Inventory
+    if user_input.lower == "i" or user_input.lower == "inv" or user_input.lower == "inventory":
+        if not inv: 
+            print("You are not carrying anything")
+        if inv:
+            print("You are carrying:")
+            print(inv)
+
     # look
-    if user_input == "look":
+    if user_input.lower == "look" or user_input.lower == "l":
         if room == 1: # lobby
             # Location: middle [0]
             if location == 0 and leaflet == False or leaflet in inv:
@@ -75,21 +83,11 @@ while True: # while is running:
                 print("You are standing in front of the secretary's door, this is locked.")
             elif location == 4:
                 print("You are standing in front of the secretary's door, it is open.")
-    # Quit
-    #elif user_input == "q" or "quit" or "exit" or "quit game" or "exit game":
-    #    print("hi")
     
+    # Quit
+    elif user_input == "q" or user_input == "quit" or user_input == "exit" or user_input == "quit game" or user_input == "exit game":
+        print("hi")
+
     # Other responses
     else:
         print(random.choice(other_responses))
-
-    
-    # Inventory # PROBLEM HERE: keeps on printing even though user_input is not correct
-    '''
-    elif user_input == "i" or "inv" or "inventory":
-        if not inv: 
-            print("You are not carrying anything")
-        elif inv:
-            print("You are carrying:")
-            print(inv)
-    '''
