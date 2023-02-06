a88ea6c24d942cb73d42c9d17c011eaac7a9c85b
Clara <clara.holst@outlook.com>
2023-02-06 10:06:18 +0100

    look

--- game.py
+++ game.py
@@ -1,21 +1,43 @@
 # Lobby #
 import random
 
-### look
-# Definitioner af variabler:
+### Definitions:
 room = 1
-leaflet = False
 location = 0
+
+# Objects
+leaflet = False
+book = False
+fire_extinguisher = False
+door_handle = False
+heating_coils = False
+secretary_door = False
+
+# Lists
 inv = []
+other_responses = ["Huh?","I beg your pardon?","What?","Come again?"]
+
+# Start
+print("KROZ: Escape the mystery mansion")
+print("Copyright (c) 2023, Escape room company, Inc. All rights reserved.")
+print("KROZ is a registered trademark of Escape room company, Inc.")
+print("Revision 1 / Serial number 000001")
+print(" ")
+print("You are standing in a lobby, there is a leaflet on the coffee table.")
+print("There is a door in the south direction.")
+print(" ")
+print("What are you going to do?")
 
 # Inputs
 while True: # while is running:
+    
     user_input = input(" ")
 
+    # look
     if user_input == "look":
         if room == 1: # lobby
             # Location: middle [0]
-            if location == 0 and leaflet != True or leaflet in inv:
+            if location == 0 and leaflet == False or leaflet in inv:
                 print("The Lobby")
                 print("You are standing in a lobby, there is a leaflet on the coffee table.")
                 print("There is a door in the south direction.")
@@ -23,6 +45,52 @@ while True: # while is running:
                 print("The Lobby")
                 print("You are standing in a lobby.")
                 print("There is a door in the south direction.")
-    else:
-        print("I beg your pardon?")
 
+            # North
+            elif location == 1 and book == False or book in inv:
+                print("You are standing in front of a bookshelf.")
+                print("All the books are lined up perfectly except for one book in particular.")
+            elif location == 1: 
+                print("You are standing in front of a bookshelf.")
+                print("All the books are lined up perfectly.")
+
+            # East
+            elif location == 2 and fire_extinguisher == False or fire_extinguisher in inv:
+                print("You are standing in front of a fire extinguisher.")
+            elif location == 2:
+                print("You are standing in front of a window.")
+                print("The fire extinguisher was here.")
+
+            # South
+            elif location == 3 and door_handle == False:
+                print("You are standing in front of a door.")
+            elif location == 3 and heating_coils == False:
+                print("You are standing in front of a door, the oven dial flashes red.")
+            elif location == 3:
+                print("You are standing in front of a door.")
+                print("The handle is missing, an oven dial appears instead.")
+
+            # West
+            elif location == 4 and secretary_door == False:
+                print("You are standing in front of the secretary's door, this is locked.")
+            elif location == 4:
+                print("You are standing in front of the secretary's door, it is open.")
+
+    elif 
+    
+    
+    # Inventory # PROBLEM HERE: keeps on printing even though user_input is not correct
+    elif user_input == "i" or "inv" or "inventory":
+        if not inv: 
+            print("You are not carrying anything")
+        elif inv:
+            print("You are carrying:")
+            print(inv)
+    
+    # Quit
+    elif user_input == "q" or "quit" or "exit" or "quit game" or "exit game":
+        print("hi")
+    
+    # Other responses
+    else:
+        print(random.choice(other_responses))
